8e64e767b02acc3e41138e728b3232b7fef422bc
ahmadqaranyfarhan <ahmadqaranyfarhan5@gmail.com>
2024-04-22 16:50:39 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+console.log(add(1, 2));
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+console.log(subtract(1, 2));
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+console.log(multiply(1, 2));
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+console.log(divide(1, 2));
+
+function increment(x) {
+  return x + 1;
+}
+console.log(increment(2));
+
+function decrement(x) {
+  return x - 1;
+}
+console.log(decrement(2));
+
+function makeInt(x) {
+  return parseInt(x, 10);
+}
+console.log(makeInt("5"));
+
+function preserveDecimal(x) {
+  return parseFloat(x);
+}
+console.log(preserveDecimal("jhgvf"));
